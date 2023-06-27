65a099da117608dd80de0cbabb2deec0e4e4a92f
KateBoghuk <137886577+KateBoghuk@users.noreply.github.com>
2023-06-27 22:17:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+let a1 = 5 % 3;
+a2 = 3 % 5;
+a3 = 5 + '3';
+a4 = '5' - 3;
+a5 = 75 + 'кг';
+a6 = 785* 653;
+a7 = 100 / 25;
+a8 = 0 * 0;
+a9 = 0 / 2;
+a10 = 89 / 0;
+a11 = 98 + 2;
+a12 = 5 - 98;
+a13 = (8 + 56 * 4) / 5;
+a14 = (9 - 12) * 7 / (5 + 2);
+a15 = +"123";
+a16 = 1 || true;
+a17 = false || true;
+a18 = true > 0;
+
+let height = 23;
+let width = 10;
+let SPryam = width * height 
+
+let h = 10;
+let VCilindra = Math.PI * Math.pow (a7, 2) * h / 4;
+
+let r = 5;
+let SKruga = Math.PI * Math.pow(5, 2);
+
+let a = 5;
+let b = 7;
+let STrap = (a + b) * h / 2;
+
+console.log("Задание 2\n " + 
+"a1 = " + a1 + "\n" +
+"a2 = " + a2 + "\n" +
+"a3 = " + a3 + "\n" +
+"a4 = " + a4 + "\n" +
+"a5 = " + a5 + "\n" +
+"a6 = " + a6 + "\n" +
+"a7 = " + a7 + "\n" +
+"a8 = " + a8 + "\n" +
+"a9 = " + a9 + "\n" +
+"a10 = " + a10 + "\n" +
+"a11 = " + a11 + "\n" +
+"a12 = " + a12 + "\n" +
+"a13 = " + a13 + "\n" +
+"a14 = " + a14 + "\n" +
+"a15 = " + a15 + "\n" +
+"a16 = " + a16 + "\n" +
+"a17 = " + a17 + "\n" +
+"a18 = " + a18 + "\n"
+)
+console.log("Задание 3 = " + SPryam + " см2 ");
+console.log("Задание 4 = " + VCilindra + " см3 ");
+console.log("Задание 5 = " + SKruga + " см2 ");
+console.log("Задание 6 = " + STrap + " см2 ");
+
